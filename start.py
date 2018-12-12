5472da73aa06798c36d64c38e1b16ec6a0b82185
Charles Shang <Creyslz@gmail.com>
2018-12-12 17:40:29 -0600

    sdfasdf

--- start.py
+++ start.py
@@ -1,53 +1,130 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+import pickle
 
 title = [
 'Prosecutors Say Trump Directed Illegal Payments During Campaign',
-2,
-3,
-4,
-5,
-6,
+'Chinese researcher claims first gene-edited babies',
+'Migrant group demands Trump either let them in or pay them each $50G to turn around: report',
+'Michael Cohen, Trump’s Ex-Lawyer Who Implicated Him in Hush-Money Scandal, Sentenced to 3 Years',
+'Korean border troops verify removal of each other\'s posts',
+'Health experts offer solutions for unintended consequences of opioid crackdown',
 ]
 website = [
 'https://www.nytimes.com/2018/12/07/nyregion/michael-cohen-sentence.html',
-2,
-3,
-4,
-5,
-6,
+'https://apnews.com/4997bb7aa36c45449b488e19ac83e86d',
+'https://www.foxnews.com/politics/caravan-migrants-in-mexico-demand-trump-either-let-them-in-u-s-or-pay-them-50000-each-to-go-back-home',
+'https://www.nytimes.com/2018/12/12/nyregion/michael-cohen-sentence-trump.html',
+'https://www.apnews.com/955d1db2ad1b4788b77d00d5a56bc273',
+'https://www.foxnews.com/health/undoing-the-harm-of-the-response-to-the-opioid-overdose-epidemic-health-experts-suggest-solutions',
 ]
 
 
 app = Flask(__name__)
 
+def getPartID():
+  return pickle.load(open('pickle/part_id.p', 'rb'))
+
+def setPartID(a):
+  pickle.dump(a, open('pickle/part_id.p', 'wb'))
+
+def loadResponses(part_id):
+  try:
+    return pickle.load(open('pickle/' + str(part_id) + '_responses.p', 'rb'))
+  except:
+    return []
+
+# response is expected to be a string.
+def saveResponse(response, part_id):
+  responses = loadResponses(part_id)
+  responses.append(response)
+  pickle.dump(responses, open('pickle/' + str(part_id) + '_responses.p', 'wb'))
+
+def loadDecisions(part_id):
+  try:
+    return pickle.load(open('pickle/' + str(part_id) + '_decisions.p', 'rb'))
+  except:
+    return []
+
+# decision is expected to be a string (Yes/No).
+def saveDecisions(decision, part_id):
+  decisions = loadDecisions(part_id)
+  decisions.append(decision)
+  print(decisions)
+  pickle.dump(decisions, open('pickle/' + str(part_id) + '_decisions.p', 'wb'))
+
+def saveFinal(final, part_id):
+  pickle.dump(final, open('pickle/' + str(part_id) + '_final.p', 'wb'))
+
+def saveSurvey(survey, part_id):
+  pickle.dump(survey, open('pickle/' + str(part_id) + '_survey.p', 'wb'))
+
+
+
+
 @app.route('/')
 def intro():
   return render_template('intro.html')
 
+@app.route('/survey.html', methods=['POST'])
+def survey():
+  return render_template('survey.html')
+
+@app.route('/intro.html', methods=['POST'])
+def instructions():
+  participant_id = getPartID() + 1
+  setPartID(participant_id)
+
+  survey_results = []
+
+  for quest in ['login', 'post', 'time', 'reasons', 'others', 'regret']:
+    survey_results.append(request.form[quest])
+  saveSurvey(survey_results, getPartID())
+  return render_template('instructions.html')
+
 @app.route('/input.html', methods=['POST'])
 def hello_world():
   step = int(request.form['step'])
+
+  if step > 1:
+    decision = request.form['action']
+    saveDecisions(decision, getPartID())
+
   link = {'link':website[step-1], 'title':title[step-1]}
   return render_template('article_page.html', link = link, step=step)
 
 @app.route('/confirm.html', methods=['POST'])
 def my_form_post():
+  participant_id = getPartID()
+  print (participant_id)
   step = int(request.form['step'])
   text = request.form['text']
+  saveResponse(text, getPartID())
   if step > 5:
     next_page = 'final.html'
   else:
     next_page = 'input.html'
-  return render_template('confirm_page.html', content=text, step=step+1, next_page=next_page)
+  if (((participant_id % 2 == 0) and step <= 3 ) or ((participant_id % 2 == 1) and step >= 4 )):
+    play_voice = 1
+  else:
+    play_voice = 0
+  return render_template('confirm_page.html', content=text, step=step+1, next_page=next_page, play_voice=play_voice)
 
 @app.route('/final.html', methods=['POST'])
 def final_check():
-  return render_template('survey.html')
+  decision = request.form['action']
+  saveDecisions(decision, getPartID())
+  responses = loadResponses(getPartID())
+  return render_template('results.html', responses = responses)
 
 @app.route('/finish.html', methods=['POST'])
 def finish():
+  final = []
+  for i in range(6):
+    final.append(request.form[str(i) + '_choice'])
+  saveFinal(final, getPartID())
+  #return str(loadDecisions(getPartID())) + '\n' + str(final)
   return render_template('thanks.html')
 
 @app.route('/test.html', methods=['POST'])
---
+++ temp.py
@@ -0,0 +1,4 @@
+import pickle
+
+a = 0
+pickle.dump(a, open('pickle/part_id.p', 'wb'))
