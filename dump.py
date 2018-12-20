a83083ce863e49a47bca2673f25af239135f3c01
Charles Shang <Creyslz@gmail.com>
2018-12-19 22:01:40 -0600

    Removed participant responses

--- dump.py
+++ dump.py
@@ -1,6 +1,9 @@
 import pickle
 
-ids = [0]
+ids = [0,1,2,3,4,5,7,8,10,11]
 for part_id in ids:
-  for content in ['_responses.p', '_decisions.p', '_final.p', '_survey.p']:
-    print(str(pickle.load(open('pickle/' + str(part_id) + content, 'rb'))))
+  #print('\n' + str(part_id))
+  for content in ['_survey.p']:#['_responses.p', '_decisions.p', '_final.p', '_survey.p']:
+    data = pickle.load(open('pickle/' + str(part_id) + content, 'rb'))
+    print(str(part_id) + ' & ' + ' & '.join(data))
+    #print(str(pickle.load(open('pickle/' + str(part_id) + content, 'rb'))))
